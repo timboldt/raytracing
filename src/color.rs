767468ad90eb6c5b15cf6211e2c2cabd0b070136
Tim Boldt <tim.boldt@gmail.com>
2024-04-28 15:58:42 -0700

    Finish implementing Color

--- src/color.rs
+++ src/color.rs
@@ -1,17 +1,27 @@
 use std::fmt;
 
+/// Represents an RGBA (Red-Green-Blue-Alpha) color value.
+///
+/// All component values must be between 0.0 and 1.0, inclusive.
 #[derive(Clone, Copy, Debug, Default)]
 pub struct Color {
     rgba: [f64; 4],
 }
 
 impl Color {
+    /// Creates a new Color, with Alpha defaulted to 1.0.
     pub fn new(r: f64, g: f64, b: f64) -> Color {
+        assert!(r >= 0.0 && r <= 1.0);
+        assert!(g >= 0.0 && g <= 1.0);
+        assert!(b >= 0.0 && b <= 1.0);
         Color {
-            rgba: [r, g, b, 0.0],
+            rgba: [r, g, b, 1.0],
         }
     }
 
+    /// Produces a string formatted according to the PPM image representation.
+    ///
+    /// TODO: This should really be part of a PPM image module instead of this module.
     pub fn as_ppm_tuple(&self) -> String {
         const SCALE: f64 = 255.999;
         format!(
@@ -23,6 +33,7 @@ impl Color {
     }
 }
 
+/// Pretty-prints a Color.
 impl fmt::Display for Color {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
@@ -30,5 +41,31 @@ impl fmt::Display for Color {
             "Color({}, {}, {}, {})",
             self.rgba[0], self.rgba[1], self.rgba[2], self.rgba[3]
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::color::Color;
+
+    #[test]
+    fn new_works() {
+        let c = Color::new(0.1, 0.2, 0.3);
+        assert_eq!(c.rgba[0], 0.1);
+        assert_eq!(c.rgba[1], 0.2);
+        assert_eq!(c.rgba[2], 0.3);
+        assert_eq!(c.rgba[3], 1.0);
+    }
+
+    #[test]
+    fn ppm_tuple_works() {
+        let c = Color::new(0.1, 0.0, 1.0);
+        assert_eq!(c.as_ppm_tuple(), "25 0 255");
+    }
+
+    #[test]
+    fn format_works() {
+        let c = Color::new(0.0, 0.5, 0.9);
+        assert_eq!(format!("{c}"), "Color(0, 0.5, 0.9, 1)");
     }
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,3 +1,5 @@
+pub type Point3 = Vec3;
+
 #[derive(Clone, Copy, Debug, Default)]
 pub struct Vec3 {
     e: [f64; 3],
@@ -7,11 +9,18 @@ impl Vec3 {
     pub fn new(e0: f64, e1: f64, e2: f64) -> Vec3 {
         Vec3 { e: [e0, e1, e2] }
     }
+
+    pub fn x(&self) -> f64 {
+        self.e[0]
+    }
+    pub fn y(&self) -> f64 {
+        self.e[1]
+    }
+    pub fn z(&self) -> f64 {
+        self.e[2]
+    }
 }
 
-//     double x() const { return e[0]; }
-//     double y() const { return e[1]; }
-//     double z() const { return e[2]; }
 
 //     vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
 //     double operator[](int i) const { return e[i]; }
